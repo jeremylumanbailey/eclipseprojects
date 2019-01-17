fa8fb0132a0be372dd19ad7686965263b28b45fa
latitude dell <jeremylumanbailey@gmail.com>
2019-01-17 17:15:02 -0500

    some changes not sure

--- src/firstProject/cmsc401.java
+++ src/firstProject/cmsc401.java
@@ -10,25 +10,34 @@ public class cmsc401 {
 
 	public static void main(String[] args) {
 		
+		boolean persist = true;
+		char commandChar;
+		
 		//Start message
 		System.out.println("Program start");
 		
 		//Scanner object created
 		Scanner sc = new Scanner(System.in); 		
 		
+		while(sc.hasNextLine() && persist) {
+
+		System.out.println(" ");
+		
 		//User input
 		String str = sc.nextLine();
 		
-		//Finds what command char it is
-		char commandChar = str.charAt(0);
+		if(str != null && !str.isEmpty()) {
+			//Finds what command char it is
+			commandChar = str.charAt(0);
+		}
+		else {
+			commandChar = 'z';
+		}
 		
 		//Creates String array of user input, each element separated by a space
 		String[] splited = str.split(" ");
 		
-//		System.out.println(splited[0]);
-		
-//		String A = splited[0];
-		
+		//Creates String array without command character
 		String[] nonIntNumbers = Arrays.copyOfRange(splited, 1, splited.length);
 		
 		//Creates int array same length of nonIntNumbers
@@ -51,49 +60,21 @@ public class cmsc401 {
 		}
 		else {
 			 System.out.println("if statement failed");
+			 persist = false;
 		}
-		
-//		System.out.println("Before: ");
-//		// Prints out each individual element in array
-//		for (int i = 0; i < nonIntNumbers.length; i++) {
-//		    System.out.println(nonIntNumbers[i]);
-//		}
-//		
-//		System.out.println("After: ");
-//		// Prints out each individual element in array
-//		for (int i = 0; i < numbers.length; i++) {
-//		    System.out.println(numbers[i]);
-//		}
-		
-	//	int y = x + 5;
-		
-//		System.out.println("x var " + y);
-//		
-//		System.out.println("splited 0 " + splited[0]);
-//		
-//		System.out.println("splited 3 " + splited[3]);
-		
+		}
+
+		System.out.println(" ");
 		System.out.println("Program complete");
 
 	}
-//	
-//	A 13 2 5 0 1 3
-	// ^ need 130
-//	B 5
-//	A 8 12 6 15 0 2 3
-	//need 12 * 6 = 72 ^
+
 	public static void AproductCommand(int[] myArray) {
 		int Product = 1;
 		int startIndex = myArray[myArray.length - 2];
 		int endIndex = myArray[myArray.length - 1];
 		int[] terms = Arrays.copyOfRange(myArray, 0, myArray.length - 2);
-//	// Prints out each individual element in array
-//		for (int i = 0; i < terms.length; i++) {
-//		    System.out.println(terms[i]);
-//		} 
-//		System.out.println("array length " + myArray.length);
-//		System.out.println("endIndex is " + endIndex);
-//		System.out.println("startIndex is " + startIndex);
+
 		for (int i = startIndex - 1; i < endIndex ; i++) {
 	    if(terms[i] != 0) {
 	    	Product = terms[i] * Product;
@@ -119,6 +100,40 @@ public class cmsc401 {
 	
 
 }
+
+
+//System.out.println(splited[0]);
+
+//String A = splited[0];
+
+//// Prints out each individual element in array
+//for (int i = 0; i < terms.length; i++) {
+//    System.out.println(terms[i]);
+//} 
+//System.out.println("array length " + myArray.length);
+//System.out.println("endIndex is " + endIndex);
+//System.out.println("startIndex is " + startIndex);
+
+
+//System.out.println("Before: ");
+//// Prints out each individual element in array
+//for (int i = 0; i < nonIntNumbers.length; i++) {
+//    System.out.println(nonIntNumbers[i]);
+//}
+//
+//System.out.println("After: ");
+//// Prints out each individual element in array
+//for (int i = 0; i < numbers.length; i++) {
+//    System.out.println(numbers[i]);
+//}
+
+//	int y = x + 5;
+
+//System.out.println("x var " + y);
+//
+//System.out.println("splited 0 " + splited[0]);
+//
+//System.out.println("splited 3 " + splited[3]);
 
 //// Prints out each individual element in array
 //for (int i = 0; i < myArray.length; i++) {
