d42a4c00798a62a54be593154a776c41c26b4041
latitude dell <jeremylumanbailey@gmail.com>
2019-02-11 13:01:58 -0500

    description added

---
+++ src/cmsc401/MyBinaryTree.java
@@ -0,0 +1,46 @@
+// @author Jeremy Bailey
+
+/*   
+	You are asked to implement a binary search tree class named MyBinaryTree that satisfies the
+	following requirements:
+		1. It will use the following class as the binary tree node.
+		class BTNode {
+		int key;
+		BTNode parent;
+		BTNode left;
+		BTNode right;
+		}
+		
+		2. It should contain but not limited to the following methods:
+		void add(int newNum) // This method will add a new integer number into the binary
+		search tree.
+		boolean delete(int delNum) // Delete the number delNum if it exists. Return true if
+		succeeds and or return false if the number does not exist.
+		boolean contains (int num) // Check whether the tree contains the number num or
+		not.
+		String inOrder() // Return the inorder traversal result (numbers are separated by spaces)
+		String preOrder() // Return the preorder traversal result (numbers are separated by
+		spaces)
+		String postOrder() // Return the postorder traversal result (numbers are separated by
+		spaces)
+		
+		3. It should have a main method that reads a sequence of numbers from the terminal and
+		print the sorted results. For example:
+		>java MyBinaryTree 5 10 7 101 42 1 21 9998 -65
+		-65 1 5 7 10 21 42 101 9998
+ */
+
+
+
+
+package cmsc401;
+
+public class MyBinaryTree {
+
+	public static void main(String[] args) {
+		
+		System.out.println("MyBinaryTree");
+
+	}
+
+}
--- src/cmsc401/MyQuicksort.java
+++ src/cmsc401/MyQuicksort.java
@@ -1,7 +1,25 @@
 // @author Jeremy Bailey
 
-package cmsc401;
+/*   
+  You are asked to implement a quicksort program named MyQuicksort that satisfies the following requirements: 
+	1. Your sorting algorithm must be quicksort.
+	
+	2. Your quicksort algorithm will always pick the left most element to be the pivot and use
+	the divide and conquer methodology to sort the array.
+	3. Your program will take a sequence of integers separated by spaces from terminal as the
+	input array.
+	
+	4. It will print the sorted array number separated by spaces.
+	
+	5. Your program shall not exceed 50 lines.
+	
+	6. For example
+	>java MyQuicksort 5 10 7 101 42 1 21 9998 -65
+	-65 1 5 7 10 21 42 101 9998
+ */
 
+
+package cmsc401;
 
 public class MyQuicksort {
 
