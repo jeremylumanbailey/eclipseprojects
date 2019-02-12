49c675b3f7186fee8d1544972c84c9778f5949b6
latitude dell <jeremylumanbailey@gmail.com>
2019-02-12 18:08:59 -0500

    latest push

--- src/cmsc401/MyQuicksort.java
+++ src/cmsc401/MyQuicksort.java
@@ -1,45 +1,24 @@
-// @author Jeremy Bailey
-
-/*   
-  You are asked to implement a quicksort program named MyQuicksort that satisfies the following requirements: 
-	1. Your sorting algorithm must be quicksort.
-	
-	2. Your quicksort algorithm will always pick the left most element to be the pivot and use
-	the divide and conquer methodology to sort the array.
-	3. Your program will take a sequence of integers separated by spaces from terminal as the
-	input array.
-	
-	4. It will print the sorted array number separated by spaces.
-	
-	5. Your program shall not exceed 50 lines.
-	
-	6. For example
-	>java MyQuicksort 5 10 7 101 42 1 21 9998 -65
-	-65 1 5 7 10 21 42 101 9998
- */
-
-
 package cmsc401;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class MyQuicksort {
 	
-	
-	
-	
-
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
+	    if (input.hasNextLine()) {
+	        String line = input.nextLine();
+	        String[] arr = line.split("\\s+");
+	        int[] vals = new int[arr.length];
+	        for (int i = 0; i < arr.length; i++) {
+	            vals[i] = Integer.parseInt(arr[i]);
+	        }
+		quickSort(vals, 0, vals.length-1);
 		
-		int Arr[] = {5, 9, 4, 6, 5, 3};
-		System.out.println("MyQuickSort");
+		System.out.println(Arrays.toString(vals));
 		
-		System.out.println(Arrays.toString(Arr));
-		
-		quickSort(Arr, 0, 5);
-		
-		System.out.println(Arrays.toString(Arr));
+	    }
 	}
 	
 	public static void quickSort(int arr[], int begin, int end) {
@@ -71,6 +50,27 @@ public class MyQuicksort {
 	 
 	    return i+1;
 	}
-
-
 }
+
+//@author Jeremy Bailey
+
+/*   
+You are asked to implement a quicksort program named MyQuicksort that satisfies the following requirements: 
+	1. Your sorting algorithm must be quicksort.
+	
+	2. Your quicksort algorithm will always pick the left most element to be the pivot and use
+	the divide and conquer methodology to sort the array.
+	
+	3. Your program will take a sequence of integers separated by spaces from terminal as the
+	input array.
+	
+	4. It will print the sorted array number separated by spaces.
+	
+	5. Your program shall not exceed 50 lines.
+	
+	6. For example
+	>java MyQuicksort 5 10 7 101 42 1 21 9998 -65
+	-65 1 5 7 10 21 42 101 9998
+*/
+
+
